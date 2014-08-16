05c7628636039479eb6503df2bcfe4fa0fcc8cec
rachnakhokhar <rachnakhokhar@gmail.com>
2014-08-17 00:07:51 +0530

    Code written to open DatePickerDialog

--- src/com/offonb/view/PostOfferActivity.java
+++ src/com/offonb/view/PostOfferActivity.java
@@ -1,38 +1,77 @@
 package com.offonb.view;
 
+import java.util.Calendar;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnFocusChangeListener;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.offonb.R;
 
-public class PostOfferActivity extends ActionBarActivity{
-	
+public class PostOfferActivity extends ActionBarActivity {
+
+	private EditText editText_StartDate, editText_EndDate;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_postoffer);
+
+		editText_StartDate = (EditText) findViewById(R.id.edittext_offer_startdate);
+		editText_StartDate.setOnFocusChangeListener(new OnFocusChangeListener() {
+		    @Override
+		    public void onFocusChange(View v, boolean hasFocus) {
+		        if(hasFocus){
+		        	selectDate(v);
+		        }else{
+		           //Hide your calender here
+		        }
+		    }
+		});
+		editText_EndDate = (EditText) findViewById(R.id.edittext_offer_enddate);
+		editText_EndDate.setOnFocusChangeListener(new OnFocusChangeListener() {
+		    @Override
+		    public void onFocusChange(View v, boolean hasFocus) {
+		        if(hasFocus){
+		        	selectDate(v);
+		        }else{
+		           //Hide your calender here
+		        }
+		    }
+		});
 	}
-	
+
+	public void selectDate(View view) {
+		DialogFragment newFragment = new SelectDateFragment();
+		newFragment.show(getSupportFragmentManager(), "DatePicker");
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.activity_menu_postoffer, menu);
-   
-        return super.onCreateOptionsMenu(menu);
+		inflater.inflate(R.menu.activity_menu_postoffer, menu);
+
+		return super.onCreateOptionsMenu(menu);
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		Log.d("Search View","Inside Options Selected");
+		Log.d("Search View", "Inside Options Selected");
 		// The action bar home/up action should open or close the drawer.
-        // ActionBarDrawerToggle will take care of this.
-       if (super.onOptionsItemSelected(item)) {
-           return true;
-       }
+		// ActionBarDrawerToggle will take care of this.
+		if (super.onOptionsItemSelected(item)) {
+			return true;
+		}
 		// Handle presses on the action bar items
 		switch (item.getItemId()) {
 		case R.id.action_post: {
@@ -41,6 +80,27 @@ public class PostOfferActivity extends ActionBarActivity{
 		}
 		default:
 			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	public void populateSetDate(int year, int month, int day) {
+		((EditText) getWindow().getCurrentFocus()).setText(month + "-" + day
+				+ "-" + year);
+	}
+
+	public class SelectDateFragment extends DialogFragment implements
+			DatePickerDialog.OnDateSetListener {
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			final Calendar calendar = Calendar.getInstance();
+			int yy = calendar.get(Calendar.YEAR);
+			int mm = calendar.get(Calendar.MONTH);
+			int dd = calendar.get(Calendar.DAY_OF_MONTH);
+			return new DatePickerDialog(getActivity(), this, yy, mm, dd);
+		}
+
+		public void onDateSet(DatePicker view, int yy, int mm, int dd) {
+			populateSetDate(yy, mm + 1, dd);
 		}
 	}
 
