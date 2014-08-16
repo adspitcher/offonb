04e3b42061088f46345ccfe9e261c754df6c6963
rachnakhokhar <rachnakhokhar@gmail.com>
2014-08-16 23:30:07 +0530

    Created Menu For Post Offer Screen.

--- src/com/offonb/view/PostOfferActivity.java
+++ src/com/offonb/view/PostOfferActivity.java
@@ -1,16 +1,47 @@
 package com.offonb.view;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
 import com.offonb.R;
 
-import android.app.Activity;
-import android.os.Bundle;
-
-public class PostOfferActivity extends Activity{
+public class PostOfferActivity extends ActionBarActivity{
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_postoffer);
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_menu_postoffer, menu);
+   
+        return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Log.d("Search View","Inside Options Selected");
+		// The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+       if (super.onOptionsItemSelected(item)) {
+           return true;
+       }
+		// Handle presses on the action bar items
+		switch (item.getItemId()) {
+		case R.id.action_post: {
+			finish();
+			return true;
+		}
+		default:
+			return super.onOptionsItemSelected(item);
+		}
 	}
 
 }
