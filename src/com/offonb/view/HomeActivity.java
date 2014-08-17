ce2bc16d44fc9ea9e5ba60816cc68c1ab55cc081
rachnakhokhar <rachnakhokhar@gmail.com>
2014-08-17 12:57:19 +0530

    Code written to fetch Brands And Categories Data on Home

--- src/com/offonb/view/HomeActivity.java
+++ src/com/offonb/view/HomeActivity.java
@@ -15,58 +15,72 @@ import android.view.MenuItem;
 
 import com.offonb.R;
 import com.offonb.adapters.HomePagerAdapter;
+import com.offonb.controllers.AppEventsController;
+import com.offonb.defines.NetworkEvents;
+import com.offonb.listeners.ActivityUpdateListener;
+import com.offonb.models.ConnectionModel;
 
 public class HomeActivity extends ActionBarActivity implements
-ActionBar.TabListener{
-	
+		ActionBar.TabListener, ActivityUpdateListener {
+
 	private ViewPager viewPager;
 	private ActionBar actionBar;
 	private HomePagerAdapter mAdapter;
-	
+	private ConnectionModel connModel;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
-		
+
 		// Initilization
-				viewPager = (ViewPager) findViewById(R.id.pager);
-				actionBar = getSupportActionBar();
-				mAdapter = new HomePagerAdapter(getSupportFragmentManager());
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		actionBar = getSupportActionBar();
+		mAdapter = new HomePagerAdapter(getSupportFragmentManager());
 
-				viewPager.setAdapter(mAdapter);
-				actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		viewPager.setAdapter(mAdapter);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		
+		connModel = AppEventsController.getInstance().getModelFacade()
+				.getConnModel();
+		connModel.registerView(this);
 
-				Resources itemTexts = getResources();
-				String[] tabs = itemTexts.getStringArray(R.array.home_tabs_array);
+		Resources itemTexts = getResources();
+		String[] tabs = itemTexts.getStringArray(R.array.home_tabs_array);
 
-				// Adding Tabs
-				for (String tab_name : tabs) {
-					actionBar.addTab(actionBar.newTab().setText(tab_name)
-							.setTabListener(this));
-				}
+		// Adding Tabs
+		for (String tab_name : tabs) {
+			actionBar.addTab(actionBar.newTab().setText(tab_name)
+					.setTabListener(this));
+		}
 
-				/**
-				 * on swiping the viewpager make respective tab selected
-				 * */
-				viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+		/**
+		 * on swiping the viewpager make respective tab selected
+		 * */
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 
-					@Override
-					public void onPageSelected(int position) {
-						// on changing the page
-						// make respected tab selected
-						actionBar.setSelectedNavigationItem(position);
-					}
+			@Override
+			public void onPageSelected(int position) {
+				// on changing the page
+				// make respected tab selected
+				actionBar.setSelectedNavigationItem(position);
+			}
 
-					@Override
-					public void onPageScrolled(int arg0, float arg1, int arg2) {
-					}
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+			}
 
-					@Override
-					public void onPageScrollStateChanged(int arg0) {
-					}
-				});
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+			}
+		});
+		
+		if( !AppEventsController.getInstance().getModelFacade().getLocalModel().isRetrievedMastedFilterData() ){
+			AppEventsController.getInstance().handleEvent(NetworkEvents.EVENT_ID_GET_BRANDS,
+				 null, viewPager);
+		}
 	}
-	
+
 	@Override
 	public void onTabReselected(Tab tab, FragmentTransaction arg1) {
 		// TODO Auto-generated method stub
@@ -86,23 +100,23 @@ ActionBar.TabListener{
 		// TODO Auto-generated method stub
 
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.activity_menu_home, menu);
-   
-        return super.onCreateOptionsMenu(menu);
+		inflater.inflate(R.menu.activity_menu_home, menu);
+
+		return super.onCreateOptionsMenu(menu);
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		Log.d("Search View","Inside Options Selected");
+		Log.d("Search View", "Inside Options Selected");
 		// The action bar home/up action should open or close the drawer.
-        // ActionBarDrawerToggle will take care of this.
-       if (super.onOptionsItemSelected(item)) {
-           return true;
-       }
+		// ActionBarDrawerToggle will take care of this.
+		if (super.onOptionsItemSelected(item)) {
+			return true;
+		}
 		// Handle presses on the action bar items
 		switch (item.getItemId()) {
 		case R.id.action_post_offer: {
@@ -114,6 +128,26 @@ ActionBar.TabListener{
 		}
 		default:
 			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	public void updateActivity(String tag) {
+		switch (connModel.getConnectionStatus()) {
+		case ConnectionModel.SUCCESS: {
+			Log.d("LoginActivity", "Inside onConnection");
+			if(tag.equals("Brands")){
+				AppEventsController.getInstance().handleEvent(NetworkEvents.EVENT_ID_GET_CATEGORIES,
+						 null, viewPager);
+			}else if(tag.equals("Categories")){
+				
+			}
+		}
+			break;
+		case ConnectionModel.ERROR: {
+			
+		}
+			break;
 		}
 	}
 
